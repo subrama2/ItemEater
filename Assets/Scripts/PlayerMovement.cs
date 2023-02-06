669a10a8619cc08a1865174d2f377c5ea0a19542
RileySetser <setserr7@gmail.com>
2023-02-05 19:03:03 -0500

    Added player character

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator anim;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            anim.SetFloat("Speed", 1);
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        } 
+        else if(Input.GetKey(KeyCode.S))
+        {
+            anim.SetFloat("Speed", -1);
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        } else
+        {
+            anim.SetFloat("Speed", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Jump", true);
+        } else
+        {
+            anim.SetBool("Jump", false);
+        }
+    }
+}
