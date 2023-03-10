ff3ce441b4f6db24b68accccb05df0d88dec7698
subrama2 <subrama2@mail.uc.edu>
2023-03-10 14:18:49 -0500

    added basic spider with script needs more functionality

--- Assets/ItemEaterScripts/EnemySpawning.cs
+++ Assets/ItemEaterScripts/EnemySpawning.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public GameObject Prefab;
+    public Rigidbody Prefab;
     public Transform SpawnPoint;
     // Start is called before the first frame update
     void OnTriggerEnter()
     {
-        Instantiate(Prefab, SpawnPoint.position, SpawnPoint.rotation);
+        Rigidbody RigidPrefab;
+        RigidPrefab = Instantiate(Prefab, SpawnPoint.position, SpawnPoint.rotation) as Rigidbody;
     }
 
 }
