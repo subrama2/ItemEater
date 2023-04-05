1c2d97e752b0eb988f43964e01323ca47151acc9
RileySetser <setserr7@gmail.com>
2023-04-05 14:25:13 -0400

    changed player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public GameObject hitBox;
+    public bool inControl = true;
 
     // Start is called before the first frame update
     void Start()
@@ -22,43 +23,45 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            anim.SetFloat("Speed", 1);
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            anim.SetFloat("Speed", 1);
-            transform.Translate(0, 0, -speed * Time.deltaTime);
-        }
-        else
-        {
-            anim.SetFloat("Speed", 0);
-        }
+        if (inControl) { 
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetFloat("Speed", 1);
+                transform.Translate(0, 0, speed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                anim.SetFloat("Speed", 1);
+                transform.Translate(0, 0, -speed * Time.deltaTime);
+            }
+            else
+            {
+                anim.SetFloat("Speed", 0);
+            }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            anim.SetFloat("HSpeed", 1);
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            anim.SetFloat("HSpeed", 1);
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        else
-        {
-            anim.SetFloat("HSpeed", 0);
-        }
+            if (Input.GetKey(KeyCode.A))
+            {
+                anim.SetFloat("HSpeed", 1);
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                anim.SetFloat("HSpeed", 1);
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                anim.SetFloat("HSpeed", 0);
+            }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            StartCoroutine(Attacking());
-        }
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(Attacking());
+            }
 
-        float mouseX = Input.GetAxis("Mouse X");
-        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+            float mouseX = Input.GetAxis("Mouse X");
+            transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+        }
     }
 
 
